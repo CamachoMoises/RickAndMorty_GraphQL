4b6ae3be271df905e79224b3f40101c6e0a8ae39
Moises Camacho <moisescamacho26@gmail.com>
2021-04-14 13:38:17 -0400

    building home view with favorite characters

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LocalStorageService } from '@shared/services/loacalStorage.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `
+  <section class="character__list" >
+    <app-characters-card *ngFor="let character of characterFav$|async" [character]="character"></app-characters-card>
+  </section>
+  `,
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HomeComponent {
+  characterFav$=this.localStoServ.charactersFav$ ;
+  constructor(
+    private localStoServ: LocalStorageService
+  ) { }
 
 }
