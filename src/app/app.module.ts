c1dd31b32dfa4bcef9b763e0c036bda8d49c5bba
Moises Camacho <moisescamacho26@gmail.com>
2021-04-15 18:39:41 -0400

    showing spinner witch HTTP interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderModule } from '@shared/components/header/header.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { ToastrModule } from 'ngx-toastr';
+
+import { SpinnerModule } from '@shared/components/spinner/spinner.module';
+import { SpinnerInterceptor } from '@shared/interceptors/spinner.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,8 +24,11 @@ import { ToastrModule } from 'ngx-toastr';
     HeaderModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    SpinnerModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from '@shared/services/spinner.services';
+
+@Component({
+  selector: 'app-spinner',
+  template: `
+  <div class="overlay" *ngIf="isLoading$ | async"  >
+    <div class="lds-hourglass"></div>
+  </div>
+  `,
+  styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent implements OnInit {
+  isLoading$=this.spinnerService.isLoading$;
+  constructor( private spinnerService: SpinnerService) { }
+
+  ngOnInit(): void {
+  }
+
+}
