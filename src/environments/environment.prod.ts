6be31981371091afae6532a2b7028a23b0d2780e
Moises Camacho <moisescamacho26@gmail.com>
2021-04-25 13:00:28 -0400

    Adding Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/quotes */
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBO_WuFaemeXcp24TyMY2Z7rev_jaj5oyM",
+    authDomain: "rick-and-morty-ac9ec.firebaseapp.com",
+    projectId: "rick-and-morty-ac9ec",
+    storageBucket: "rick-and-morty-ac9ec.appspot.com",
+    messagingSenderId: "821759660023",
+    appId: "1:821759660023:web:f5768d66b0795588c4e8f2",
+    measurementId: "G-Y90HVRH7FY"
+  }
 };
